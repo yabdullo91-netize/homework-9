ded05328b262df501c302ddde7606d18cca13d26
abdulloyakubov <yabdullo91@gmail.com>
2026-01-16 18:08:47 +0500

    two

--- Domain/Models/Department.cs
+++ Domain/Models/Department.cs
@@ -4,7 +4,7 @@ namespace Domain.Models;
 
 public class Department
 {
-    public string Name{get;set;}
-    public string Description{get;set;}
+    public string Name{get;set;}=string.Empty;
+    public string Description{get;set;}=string.Empty;
     public Employee Manager{get;set;}
 }
